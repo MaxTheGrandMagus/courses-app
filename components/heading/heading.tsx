8e90f3fd37bfb4efc8cee74f5714ffcf1ccd7bc6
Max <veyronmclord@gmail.com>
2022-10-03 23:26:53 +0600

    feat: add heading component, add styles module

---
+++ components/heading/heading.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { HeadingProps } from './heading.props';
+import styles from './heading.module.css';
+
+export const Heading = ({ tag, children }: HeadingProps):JSX.Element => {
+  switch (tag) {
+    case 'h1':
+      return <h1 className={styles.h1}>{children}</h1>;
+    case 'h2':
+      return <h2 className={styles.h2}>{children}</h2>;
+    case 'h3':
+      return <h3 className={styles.h3}>{children}</h3>;
+    case 'h4':
+      return <h4 className={styles.h4}>{children}</h4>;
+    case 'h5':
+      return <h5 className={styles.h5}>{children}</h5>;
+    case 'h6':
+      return <h6 className={styles.h6}>{children}</h6>;
+    default:
+      return <></>;
+  }
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
 import type { NextPage } from 'next';
+import { Heading } from '../components';
 
 const Home: NextPage = (): JSX.Element => {
   return (
     <div>
-      
+      <Heading tag='h1'>Текст</Heading>
     </div>
   );
 };

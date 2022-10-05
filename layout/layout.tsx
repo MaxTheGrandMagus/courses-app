d83f0c567333918b52c90bc17ae73dd173b74e53
Max <veyronmclord@gmail.com>
2022-10-05 15:06:42 +0600

    feat: make general layout, add layout styles

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -8,16 +8,14 @@ import { Footer } from './footer/footer';
 
 const Layout = ({ children }: LayoutProps):JSX.Element => {
   return (
-    <>
-      <Header />
-      <div>
-        <Sidebar />
-        <div>
-          {children}
-        </div>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <div className={styles.body}>
+        {children}
       </div>
-      <Footer />
-    </>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 

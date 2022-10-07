8c13c39c2d124fad07911a5f886042511c64581c
Max <veyronmclord@gmail.com>
2022-10-07 21:39:55 +0600

    feat: add first ssg pages connected with backend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import type { NextPage } from 'next';
+import type { GetStaticProps } from 'next';
 import React from 'react';
 import { Button, Heading, Paragraph, Rating, Tag } from '../components';
 import { withLayout } from '../layout/layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
 
-const Home: NextPage = (): JSX.Element => {
+const Home = ({ menu, firstCategory }: HomeProps): JSX.Element => {
   const [rating, setRating] = React.useState<number>(4);
   return (
     <>
@@ -18,8 +20,31 @@ const Home: NextPage = (): JSX.Element => {
       <Tag size='s' color='green'>Green</Tag>
       <Tag color='primary'>Primary</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
+      <ul>
+        {menu.map(m => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 };
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+  return {
+    props: {
+      menu,
+      firstCategory,
+    },
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}

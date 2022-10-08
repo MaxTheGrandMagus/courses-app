0154231655c0f392a00b26a6400e4dccc1deefc0
Max <veyronmclord@gmail.com>
2022-10-08 20:37:21 +0600

    feat: add routing for other top level categories

---
+++ helpers/helpers.tsx
@@ -0,0 +1,13 @@
+import { TopLevelCategory } from '../interfaces/page.interface';
+import { FirstLevelMenuItem } from '../interfaces/menu.interface';
+import CoursesIcon from '../assets/icons/courses.svg';
+import ServicesIcon from '../assets/icons/services.svg';
+import BooksIcon from '../assets/icons/books.svg';
+import ProductsIcon from '../assets/icons/products.svg';
+
+export const firstLevelMenu: FirstLevelMenuItem[] = [
+  { route: 'courses', name: 'Курсы', icon: <CoursesIcon />, id: TopLevelCategory.Courses },
+  { route: 'services', name: 'Сервисы', icon: <ServicesIcon />, id: TopLevelCategory.Services },
+  { route: 'books', name: 'Книги', icon: <BooksIcon />, id: TopLevelCategory.Books },
+  { route: 'products', name: 'Продукты', icon: <ProductsIcon />, id: TopLevelCategory.Products },
+];

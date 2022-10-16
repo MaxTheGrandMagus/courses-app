b534c9abe39136901ec09b56e34f770701d9a32e
Max <veyronmclord@gmail.com>
2022-10-16 16:11:29 +0600

    feat: add image from next, add configuration for image domains to next.config

--- components/product/product.tsx
+++ components/product/product.tsx
@@ -5,12 +5,18 @@ import { Card } from '../card/card';
 import { Rating } from '../rating/rating';
 import { Button, Divider, Tag } from '..';
 import { declOfNum, priceRu } from '../../helpers/helpers';
+import Image from 'next/image';
 
 export const Product = ({ product, className, ...props }: ProductProps):JSX.Element => {
   return (
     <Card className={styles.product}>
       <div className={styles.logo}>
-        <img src={process.env.NEXT_PUBLIC_DOMAIN + product.image} alt={product.title} />
+        <Image 
+          src={process.env.NEXT_PUBLIC_DOMAIN + product.image} 
+          alt={product.title}
+          width={70}
+          height={70}
+        />
       </div>
       <div className={styles.title}>{product.title}</div>
       <div className={styles.price}>
--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['courses-top.ru']
+  },
   webpack(config, options) {
     config.module.rules.push({
       loader: '@svgr/webpack',

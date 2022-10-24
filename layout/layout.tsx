82bbf138624ae7160adff3acdecf553f321b93e1
Max <veyronmclord@gmail.com>
2022-10-25 00:20:52 +0600

    feat: add accessibility to sidebar menu by keyboard

--- layout/layout.tsx
+++ layout/layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { KeyboardEvent, useRef, useState } from 'react';
 import { LayoutProps } from './layout.props';
 import styles from './layout.module.css';
 import { Header } from './header/header';
@@ -6,13 +6,35 @@ import { Sidebar } from './sidebar/sidebar';
 import { Footer } from './footer/footer';
 import { AppContextProvider, IAppContext } from '../context/app.context';
 import { Up } from '../components';
+import classnames from 'classnames';
 
 const Layout = ({ children }: LayoutProps):JSX.Element => {
+  const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] = useState<boolean>(false);
+  const bodyRef = useRef<HTMLDivElement>(null);
+
+  const handleSkipLinkClick = (key: KeyboardEvent) => {
+    if (key.code === 'Space' || key.code === 'Enter') {
+      key.preventDefault();
+      bodyRef.current?.focus();
+    }
+    setIsSkipLinkDisplayed(false);
+  };
+
   return (
     <div className={styles.wrapper}>
+      <a
+        onFocus={() => setIsSkipLinkDisplayed(true)}
+        onKeyDown={handleSkipLinkClick}
+        tabIndex={1} 
+        className={classnames(styles.skipLink, {
+          [styles.skipLinkDisplayed]: isSkipLinkDisplayed,
+        })}
+      >
+        Сразу к содержанию
+      </a>
       <Header className={styles.header} />
       <Sidebar className={styles.sidebar} />
-      <div className={styles.body}>
+      <div className={styles.body} ref={bodyRef} tabIndex={0}>
         {children}
       </div>
       <Footer className={styles.footer} />
--- layout/menu/menu.tsx
+++ layout/menu/menu.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { KeyboardEvent, useContext } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import styles from './menu.module.css';
@@ -45,6 +45,13 @@ export const Menu = (): JSX.Element => {
     }));
   };
 
+  const openSecondLevelKey = (key: KeyboardEvent, secondCategory: string) => {
+    if (key.code === 'Space' || key.code === 'Enter') {
+      key.preventDefault();
+      openSecondLevel(secondCategory);
+    }
+  };
+
   const buildFirstLevel = () => {
     return (
       <>
@@ -76,7 +83,14 @@ export const Menu = (): JSX.Element => {
           }
           return (
             <div key={m._id.secondCategory}>
-              <div className={styles.secondLevel} onClick={() => openSecondLevel(m._id.secondCategory)}>{m._id.secondCategory}</div>
+              <div 
+                tabIndex={0} 
+                onClick={() => openSecondLevel(m._id.secondCategory)}
+                onKeyDown={(key: KeyboardEvent) => openSecondLevelKey(key, m._id.secondCategory)}
+                className={styles.secondLevel} 
+              >
+                {m._id.secondCategory}
+              </div>
               <motion.div
                 layout
                 variants={variants}
@@ -84,7 +98,7 @@ export const Menu = (): JSX.Element => {
                 animate={m.isOpened ? 'visible' : 'hidden'}
                 className={classnames(styles.secondLevelBlock)}
               >
-                {buildThirdLevel(m.pages, menuItem.route)}
+                {buildThirdLevel(m.pages, menuItem.route, m.isOpened ?? false)}
               </motion.div>
             </div>
           );
@@ -93,12 +107,12 @@ export const Menu = (): JSX.Element => {
     );
   };
 
-  const buildThirdLevel = (pages: PageItem[], route: string) => {
+  const buildThirdLevel = (pages: PageItem[], route: string, isOpened: boolean) => {
     return (
       pages.map(p => (
         <motion.div key={p._id} variants={variantsChildren}>
           <Link href={`/${route}/${p.alias}`}>
-            <a className={classnames(styles.thirdLevel, {
+            <a tabIndex={isOpened ? 0 : -1} className={classnames(styles.thirdLevel, {
               [styles.thirdLevelActive]: `/${route}/${p.alias}` === router.asPath,
             })}>
               {p.category}

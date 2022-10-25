c5d0746e7017c14300f90408530e2cef0e4b52ac
Max <veyronmclord@gmail.com>
2022-10-26 01:30:57 +0600

    feat: add yandex metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Head from 'next/head';
+import ym, { YMInitializer } from 'react-yandex-metrika';
 
 function MyApp({ Component, pageProps, router }: AppProps):JSX.Element {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  });
+
   return <>
     <Head>
       <title>Courses App</title>
@@ -10,9 +17,15 @@ function MyApp({ Component, pageProps, router }: AppProps):JSX.Element {
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+      <link rel="preconnect" href="https://mc.yandex.ru" />
       <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
       <meta property='og:locale' content="ru-RU" />
     </Head>
+    <YMInitializer 
+      accounts={[]} 
+      options={{ webvisor: true, defer: true }} 
+      version="2" 
+    />
     <Component {...pageProps} />
   </>;
 }

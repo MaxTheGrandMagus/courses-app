78c34f0d493c742b20ef869c22dcf24b24bb067d
Max <veyronmclord@gmail.com>
2022-10-26 01:14:50 +0600

    feat: add meta tags for pages, add og meta tags

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Head from 'next/head';
 
-function MyApp({ Component, pageProps }: AppProps):JSX.Element {
+function MyApp({ Component, pageProps, router }: AppProps):JSX.Element {
   return <>
     <Head>
       <title>Courses App</title>
@@ -10,6 +10,8 @@ function MyApp({ Component, pageProps }: AppProps):JSX.Element {
       <link rel="icon" href="/favicon.ico" />
       <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='anonymous' />
+      <meta property='og:url' content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+      <meta property='og:locale' content="ru-RU" />
     </Head>
     <Component {...pageProps} />
   </>;
